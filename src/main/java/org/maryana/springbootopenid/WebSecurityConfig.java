36e208753c79227f04eabf78c848f00a8097e24e
marmar <marianamartyniuk2001@gmail.com>
2022-01-11 16:54:39 +0100

    Add logout file

--- src/main/java/org/maryana/springbootopenid/WebSecurityConfig.java
+++ src/main/java/org/maryana/springbootopenid/WebSecurityConfig.java
@@ -4,11 +4,15 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @Configuration
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
